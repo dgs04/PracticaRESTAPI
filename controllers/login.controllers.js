65266e13f0f1cc2e8274285a0e2819545f2a683f
dgs04 <A01285960@tec.mx>
2026-04-21 10:37:19 -0600

    Conexion de API con MongoDB Atlas y correccion de controladores

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,11 +1,37 @@
 import User from '../models/users.models.js';
-export const login = async (req, res) => { 
-    const{ username, password } = req.body;
-    const user = await User.findOne
-    ({ username:username });
-    if (user.password === password) {
-        res.json({login:true, msg:'Login successful', user:user});
-    } else {
-        res.status(404).json({login:false, msg:'Invalid credentials', user:{}});
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        isLogin: false,
+        msg: 'Usuario no encontrado',
+        user: {}
+      });
     }
+
+    if (user.password !== password) {
+      return res.status(401).json({
+        isLogin: false,
+        msg: 'Credenciales inválidas',
+        user: {}
+      });
+    }
+
+    res.json({
+      isLogin: true,
+      msg: 'Login successful',
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      isLogin: false,
+      msg: 'Error en el servidor',
+      error: error.message
+    });
+  }
 };
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,28 +1,54 @@
 import User from '../models/users.models.js';
+
 export const getUsers = async (req, res) => {
+  try {
     const users = await User.find();
     res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const getUser = async (req, res) => {
+  try {
     const id = req.params.id;
     const user = await User.findById(id);
     res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const postUser = async (req, res) => {
+  try {
     const { name, username, password } = req.body;
     const user = new User({ name, username, password });
     await user.save();
     res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const putUser = async (req, res) => {
-    const user = await User.findByIdandUpdate(req.params.id, {name, username, password}, {new: true});
+  try {
+    const { name, username, password } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { name, username, password },
+      { new: true }
+    );
     res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const delUser = async (req, res) => {
+  try {
     await User.findByIdAndDelete(req.params.id);
-    res.json({message: 'User deleted'});
+    res.json({ message: 'User deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
